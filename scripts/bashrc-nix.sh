14a6e98dcb6c9d594ce594ce7fc214a8e44e21d3
Hykilpikonna <me@hydev.org>
2021-08-24 15:19:17 -0400

    [O] Fix nix-git-update

--- scripts/bashrc-nix.sh
+++ scripts/bashrc-nix.sh
@@ -4,19 +4,28 @@ alias rebuild-gc="rebuild; gc"
 
 # Update git
 nix-git-update() {
+    pushd /etc/nixos
+    
     # Make sure there aren't any other changes
     if git diff-index --quiet HEAD --; then
         # No changes
-        pushd /etc/nixos
         update-nix-fetchgit *.nix
-        rebuild-gc
-        git add *.nix
-        git commit -m "[U] Update fetchgit refs"
-        git push
-        popd
+        
+        # If there are changes after updating
+        if git diff-index --quiet HEAD --; then
+            rebuild-gc
+            git add *.nix
+            git commit -m "[U] Update fetchgit refs"
+            git push
+            echo "Successfully updated fetchgit refs"
+        else
+            echo "There aren't any updates"
+        fi
     else
         # Changes
         echo "Error: There are uncommitted changes"
         git status
     fi
+    
+    popd
 }
