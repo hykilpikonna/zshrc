4486a65bfcbd9150601188549fe375487d7b7128
Hykilpikonna <me@hydev.org>
2021-09-16 23:29:07 -0400

    [+] Create prompt helper script

--- scripts/includes/prompt.sh
+++ scripts/includes/prompt.sh
@@ -1,6 +1,14 @@
 # Bash PS1 (Not updated)
 PS1='\n\[\e[m\][\[\e[35m\]\D{%y-%m-%d} \t\[\e[m\]] [\[\e[34m\]\h\[\e[m\]] [\[\e[33m\]\u\[\e[m\]] \[\e[37m\]\w \n\[\e[m\]$ '
 
+# Prompt helper
+alias prompt="$SCR/helpers/prompt.py"
+PROMPT_RAW=""
+prompt-set() {
+    PROMPT_RAW=$(prompt "$PROMPT_RAW" set $1 $2)
+}
+alias prompt-show="prompt $PROMPT_RAW show"
+
 # ZSH
 DEFAULT_USER="hykilpikonna"
 PROMPT=$(color "&n&5%D{%a %m-%d %H:%M}&r | &1%m&r |")
