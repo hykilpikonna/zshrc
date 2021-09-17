b92abfbd67cbb65a2d5a1962903c9bd196fe6afc
Hykilpikonna <me@hydev.org>
2021-09-17 10:08:19 -0400

    [+] Create prompt-update command

--- scripts/includes/prompt.sh
+++ scripts/includes/prompt.sh
@@ -4,11 +4,14 @@ PS1='\n\[\e[m\][\[\e[35m\]\D{%y-%m-%d} \t\[\e[m\]] [\[\e[34m\]\h\[\e[m\]] [\[\e[
 # Prompt helper
 alias prompt="$SCR/helpers/prompt.py"
 PROMPT_RAW=""
+prompt-show() {
+    prompt "$PROMPT_RAW" show
+}
 prompt-set() {
     PROMPT_RAW=$(prompt "$PROMPT_RAW" set $1 $2)
 }
-prompt-show() {
-    prompt "$PROMPT_RAW" show
+prompt-update() {
+    PROMPT=$(prompt-show)
 }
 
 ########### Build a zsh prompt
@@ -25,7 +28,7 @@ prompt-set 30 "&e%n&r "
 prompt-set 40 "&r%~ "
 # New line after the prompt header
 prompt-set 1000 "&n"
-# Before input
+# Prompt before input
 prompt-set 1100 "> "
 # Create prompt
-PROMPT=$(prompt-show)
+prompt-update
