5db117502f83d388cd7b53b9551d9488307eb995
Hykilpikonna <me@hydev.org>
2021-09-16 21:32:44 -0400

    [O] Detect archlinux in bashrc

--- scripts/bashrc-archlinux.sh
+++ scripts/bashrc-archlinux.sh
@@ -1,5 +1,6 @@
-
-alias install='pacman -S'
-alias uninstall='pacman -Rsn'
-alias listunused='pacman -Qdtq'
-alias aurinst='yay -S'
+if command -v pacman &> /dev/null; then
+  alias install='pacman -S'
+  alias uninstall='pacman -Rsn'
+  alias listunused='pacman -Qdtq'
+  alias aurinst='yay -S'
+fi
