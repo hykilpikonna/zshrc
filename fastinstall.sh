802ae6166367508bc51995df3ec0abb38772196c
Hykilpikonna <me@hydev.org>
2021-09-18 11:37:53 -0400

    [+] Create one key install script

---
+++ fastinstall.sh
@@ -0,0 +1,20 @@
+# Go to home directory
+pushd $HOME
+
+# Clone repo
+git clone "https://github.com/hykilpikonna/zshrc"
+
+# Addline function: add a line to a file if the line doesn't already exist
+addline() {
+  grep -qxF "$2" "$1" || echo "$2" >> $1
+}
+
+# Add lines
+addline .zshrc 'SCR="$HOME/zshrc/scripts"'
+addline .zshrc '. $SCR/zshrc.sh'
+
+# Source file
+. .zshrc
+
+# Return to the previous directory
+popd
