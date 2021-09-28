794ea059c14641642910ac5e1959c5ae1ed96c23
Hykilpikonna <me@hydev.org>
2021-09-27 23:56:51 -0400

    [+] Enable ls coloring for mac

--- scripts/includes/mac.sh
+++ scripts/includes/mac.sh
@@ -1,6 +1,6 @@
 # Mac-only commands
 if [[ $OSTYPE == 'darwin'* ]]; then
-    alias ls='ls -h'
+    alias ls='ls -hG'
     alias ports="netstat -ap tcp | grep -i \"listen\""
     alias trash="rmtrash"
     alias checkrain="/Applications/checkra1n.app/Contents/MacOS/checkra1n"
