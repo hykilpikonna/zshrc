c1d58672d91f233f6939536e54a4cda350ca3efe
Hykilpikonna <me@hydev.org>
2021-11-25 23:09:31 -0500

    [+] Extend color command to include RGB coloring

--- scripts/helpers/prompt.py
+++ scripts/helpers/prompt.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 import json
 import sys
-import time
-from typing import Tuple, List
+
+
+def ansirgb(r: int, g: int, b: int, foreground: bool = True) -> str:
+    c = '38' if foreground else '48'
+    return f'\033[{c};2;{r};{g};{b}m'
 
 
 def replace_color(msg: str) -> str:
     replacements = ["&0/\033[0;30m", "&1/\033[0;34m", "&2/\033[0;32m", "&3/\033[0;36m", "&4/\033[0;31m", "&5/\033[0;35m", "&6/\033[0;33m", "&7/\033[0;37m", "&8/\033[1;30m", "&9/\033[1;34m", "&a/\033[1;32m", "&b/\033[1;36m", "&c/\033[1;31m", "&d/\033[1;35m", "&e/\033[1;33m", "&f/\033[1;37m", "&r/\033[0m", "&n/\n"]
     for r in replacements:
         msg = msg.replace(r[:2], r[3:])
+    
+    while '&gf(' in msg or '&gb(' in msg:
+        i = msg.index('&gf(') if '&gf(' in msg else msg.index('&gb(')
+        end = msg.index(')', i)
+        code = msg[i + 4:end]
+        fore = msg[i + 2] == 'f'
+
+        if code.startswith('#'):
+            rgb = tuple(int(code.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+        else:
+            code = code.replace(',', ' ').replace(';', ' ').replace('  ', ' ')
+            rgb = tuple(int(c) for c in code.split(' '))
+
+        msg = msg[:i] + ansirgb(*rgb, foreground=fore) + msg[end + 1:]
+
     return msg
 
 
@@ -28,12 +46,16 @@ def set():
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-
+    # start_time = time.time()
     args = sys.argv[1:]
     parts_raw = args.pop(0)
-    parts: List[Tuple[int, str]] = json.loads(parts_raw) if parts_raw != '' else []
     cmd = args.pop(0).lower()
+
+    if cmd == 'color':
+        print(replace_color(parts_raw))
+        exit()
+
+    parts: list[tuple[int, str]] = json.loads(parts_raw) if parts_raw != '' else []
 
     if cmd == 'show':
         show()
