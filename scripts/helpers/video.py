a2061d377493d747b31e0d0e0a5de1a67636c15c
Hykilpikonna <me@hydev.org>
2021-12-11 09:58:21 -0500

    [+] Additional parameters

--- scripts/helpers/video.py
+++ scripts/helpers/video.py
@@ -6,7 +6,7 @@ from datetime import datetime
 if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args) < 2:
-        print('Usage: compress [rename/cpu/gpu] <input> [crf=20]')
+        print('Usage: compress [rename/cpu/gpu/cpucmd/gpucmd] <input> [crf=24] [additional args]')
 
     # Command to rename all screen recordings
     if args[0] == 'rename':
@@ -27,11 +27,20 @@ if __name__ == '__main__':
     i = args[1]
     crf = args[2] if len(args) > 2 else '24'
     out = i[:i.rindex('.')] + f'.{processor[0]}265'
+    cmd = 'cmd' in processor
+    if cmd:
+        processor = processor.replace('cmd', '')
     if processor == 'cpu':
         out += f'-{crf}'
     out += '.mp4'
+    additional_args = ' '.join(args[3:] if len(args) > 3 else [])
 
     if processor == 'cpu':
-        os.system(f'ffmpeg -i "{i}" -vcodec libx265 -crf {crf} "{out}"')
+        c = f'ffmpeg -i "{i}" -vcodec libx265 -crf {crf} {additional_args} "{out}"'
     if processor == 'gpu':
-        os.system(f'ffmpeg -i "{i}" -c:v hevc_videotoolbox -b:v 4000k "{out}"')
+        c = f'ffmpeg -i "{i}" -c:v hevc_videotoolbox -b:v 1000k {additional_args} "{out}"'
+    
+    if cmd:
+        print(c)
+    else:
+        os.system()
