01bae45915d05ddb79b91767b1cc23dde78e6563
Hykilpikonna <me@hydev.org>
2021-12-11 19:41:05 -0500

    [O] Use python function structure for vcomp

--- scripts/zshrc.sh
+++ scripts/zshrc.sh
@@ -119,6 +119,7 @@ cut() {
     ffmpeg -i $1 -codec copy -ss $start -t $2 Cut\ $1
 }
 alias vcomp="$SCR/helpers/video.py"
+alias vcompy="ipython -i $SCR/helpers/video.py"
 
 # include if it exists
 [ -f $HOME/extra.rc.sh ] && . $HOME/extra.rc.sh
