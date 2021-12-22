a2945985ae722e89bc1fdbe95e0fb05ad0a101e7
Hykilpikonna <me@hydev.org>
2021-12-22 02:00:20 -0500

    [+] Python paths

--- scripts/includes/home-mac.sh
+++ scripts/includes/home-mac.sh
@@ -5,7 +5,6 @@ if [[ $OSTYPE == 'darwin'* ]] && [ -d "/Volumes/External" ]; then
     alias minecraft="pushd $MC_DIR; java17 -jar $MC_DIR/HMCL-*.jar; popd"
 
     # Paths
-    export PATH="$PATH:/Users/hykilpikonna/.pyenv/versions/3.8.0/bin"
     export DOTNET_ROOT="/usr/local/opt/dotnet/libexec"
     # export PATH="/Volumes/MacData/SageMath:$PATH"
     export PATH="$PATH:/Users/hykilpikonna/.gem/ruby/2.6.0/bin" # https://stackoverflow.com/a/53388305/7346633
@@ -14,4 +13,10 @@ if [[ $OSTYPE == 'darwin'* ]] && [ -d "/Volumes/External" ]; then
     export PATH="${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
     export NDK_HOME="/Volumes/MacData/Resources/android-ndk-r21d"
     export NDK_ROOT=$NDK_HOME
+    export PATH="/usr/local/opt/node@14/bin:$PATH"
+
+    # Use Python3.9 by default
+    export PATH="/usr/local/opt/python@3.8/bin:$PATH"
+    export PATH="/usr/local/opt/python@3.10/bin:$PATH"
+    export PATH="/usr/local/opt/python@3.9/bin:$PATH"
 fi
