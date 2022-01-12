e6d7b4f38d4d5296e20d60cc08f2e45145b72a1e
Hykilpikonna <me@hydev.org>
2022-01-13 02:47:45 +0800

    [U] Only import PSColor if exists

--- powershell.ps1
+++ powershell.ps1
@@ -66,32 +66,34 @@ function cropv($file, $len)
 }
 
 # ls coloring
-Import-Module PSColor
-$global:PSColor = @{
-    File = @{
-        Default    = @{ Color = 'White' }
-        Directory  = @{ Color = 'Blue'}
-        Hidden     = @{ Color = 'DarkGray'; Pattern = '^\.' } 
-        Code       = @{ Color = 'Magenta'; Pattern = '\.(java|c|cpp|cs|js|css|html)$' }
-        Executable = @{ Color = 'Red'; Pattern = '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg)$' }
-        Text       = @{ Color = 'Yellow'; Pattern = '\.(txt|cfg|conf|ini|csv|log|config|xml|yml|md|markdown)$' }
-        Compressed = @{ Color = 'Green'; Pattern = '\.(zip|tar|gz|rar|jar|war)$' }
-    }
-    Service = @{
-        Default = @{ Color = 'White' }
-        Running = @{ Color = 'DarkGreen' }
-        Stopped = @{ Color = 'DarkRed' }     
-    }
-    Match = @{
-        Default    = @{ Color = 'White' }
-        Path       = @{ Color = 'Cyan'}
-        LineNumber = @{ Color = 'Yellow' }
-        Line       = @{ Color = 'White' }
-    }
-	NoMatch = @{
-        Default    = @{ Color = 'White' }
-        Path       = @{ Color = 'Cyan'}
-        LineNumber = @{ Color = 'Yellow' }
-        Line       = @{ Color = 'White' }
+if (Get-Module -ListAvailable -Name PSColor) {
+    Import-Module PSColor
+    $global:PSColor = @{
+        File = @{
+            Default    = @{ Color = 'White' }
+            Directory  = @{ Color = 'Blue'}
+            Hidden     = @{ Color = 'DarkGray'; Pattern = '^\.' } 
+            Code       = @{ Color = 'Magenta'; Pattern = '\.(java|c|cpp|cs|js|css|html)$' }
+            Executable = @{ Color = 'Red'; Pattern = '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg)$' }
+            Text       = @{ Color = 'Yellow'; Pattern = '\.(txt|cfg|conf|ini|csv|log|config|xml|yml|md|markdown)$' }
+            Compressed = @{ Color = 'Green'; Pattern = '\.(zip|tar|gz|rar|jar|war)$' }
+        }
+        Service = @{
+            Default = @{ Color = 'White' }
+            Running = @{ Color = 'DarkGreen' }
+            Stopped = @{ Color = 'DarkRed' }     
+        }
+        Match = @{
+            Default    = @{ Color = 'White' }
+            Path       = @{ Color = 'Cyan'}
+            LineNumber = @{ Color = 'Yellow' }
+            Line       = @{ Color = 'White' }
+        }
+        NoMatch = @{
+            Default    = @{ Color = 'White' }
+            Path       = @{ Color = 'Cyan'}
+            LineNumber = @{ Color = 'Yellow' }
+            Line       = @{ Color = 'White' }
+        }
     }
 }
