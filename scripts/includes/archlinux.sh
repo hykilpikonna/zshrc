4fd1700414cead707f882d13e1ac43e86b8f7b56
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-08-17 12:23:02 -0400

    [+] Java path

--- scripts/includes/archlinux.sh
+++ scripts/includes/archlinux.sh
@@ -4,3 +4,15 @@ if command -v pacman &> /dev/null; then
     alias listunused='pacman -Qdtq'
     alias aurinst='yay -S'
 fi
+
+if [ -f "/etc/arch-release" ]; then
+    # Java paths
+    export JDK8="/usr/lib/jvm/java-8-openjdk/"
+    export JDK11="/usr/lib/jvm/java-11-openjdk/"
+    export JDK17="/usr/lib/jvm/java-17-openjdk/"
+    alias java8="${JDK8}/bin/java"
+    alias java11="${JDK11}/bin/java"
+    alias java17="${JDK17}/bin/java"
+    export JAVA_HOME=${JDK11}
+    export PATH="${JDK11}/bin:$PATH"
+fi
