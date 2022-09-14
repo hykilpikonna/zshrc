33f7d8ebc8fd56efd9d6ea260438caf0f11f2e81
Hykilpikonna <me@hydev.org>
2022-09-13 23:55:07 -0400

    [F] pacman should be sudo

--- arch.setup.sh
+++ arch.setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Stop on error
+set -e
+
 # Install yay
-pacman -S --needed git base-devel
+sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git /tmp/yay
 cd /tmp/yay
 makepkg -si
