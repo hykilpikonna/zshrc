cd5984ef694621fd4e690d4ed430b49dcc2c4266
Azalea <me@hydev.org>
2022-09-15 20:21:59 -0400

    [+] conda-init on Arch

--- scripts/includes/archlinux.sh
+++ scripts/includes/archlinux.sh
@@ -1,5 +1,5 @@
 if command -v pacman &> /dev/null; then
-    alias install='pacman -S'
+    alias install='pacman -Sy'
     alias uninstall='pacman -Rsn'
     alias listunused='pacman -Qdtq'
     alias aurinst='yay -S'
@@ -18,4 +18,22 @@ if [ -f "/etc/arch-release" ]; then
 
     # GPG Init
     alias gpg-init="echo 'hi' | gpg --status-fd=2 -bsau E289FAC0DA92DD2B"
+
+    CONDA_PATH="/opt/miniconda3"
+
+    # Conda initialize
+    conda-init() {
+        # !! Contents within this block are managed by 'conda init' !!
+        __conda_setup="$("$CONDA_PATH/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+        if [ $? -eq 0 ]; then
+            eval "$__conda_setup"
+        else
+            if [ -f "$CONDA_PATH/etc/profile.d/conda.sh" ]; then
+                . "$CONDA_PATH/etc/profile.d/conda.sh"
+            else
+                export PATH="$CONDA_PATH/bin:$PATH"
+            fi
+        fi
+        unset __conda_setup
+    }
 fi
