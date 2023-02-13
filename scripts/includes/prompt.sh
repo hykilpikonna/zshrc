918b10c7b2f69416998432130a6ed9554b35724d
Azalea Gui <me@hydev.org>
2023-02-13 15:24:54 -0500

    [O] Simplify prompt

--- scripts/includes/prompt.sh
+++ scripts/includes/prompt.sh
@@ -21,17 +21,19 @@ pcolor() {
 
 # Build a zsh prompt
 prompt-reset() {
+    host="$(sed "s/HyDEV-//g" <<< "$HOST")"
+
     # New line first
     prompt-set 0 "&n"
     # Time stamp
     prompt-set 10 "&5%D{%a %m-%d %H:%M}&r "
-    [[ "$HOST" != "HyDEV" ]] && prompt-set 10 "&gf(#55CDFC)%D{%a} &gf(#F7A8B8)%D{%m-}&f%D{%d} &gf(#F7A8B8)%D{%H:}&gf(#55CDFC)%D{%M}&r "
+    [[ "$host" != "HyDEV" ]] && prompt-set 10 "&gf(#55CDFC)%D{%a} &gf(#F7A8B8)%D{%m-}&f%D{%d} &gf(#F7A8B8)%D{%H:}&gf(#55CDFC)%D{%M}&r "
     # Hostname
-    prompt-set 20 "&1%m&r "
-    [[ "$HOST" == "HyDEV" ]] && prompt-set 20 "&gf(#55CDFC)H&gf(#F7A8B8)y&fD&gf(#F7A8B8)E&gf(#55CDFC)V&r "
+    prompt-set 20 "&1$host&r "
+    [[ "$host" == "HyDEV" ]] && prompt-set 20 "&gf(#55CDFC)H&gf(#F7A8B8)y&fD&gf(#F7A8B8)E&gf(#55CDFC)V&r "
     # Username, or show a cat if I'm hykilpikonna
     prompt-set 30 "&e%n&r "
-    [[ "$USER" == "hykilpikonna" ]] && prompt-set 30 "🐱 "
+    [[ "$USER" == "hykilpikonna" || "$USER" == "azalea" ]] && prompt-set 30 "🐱 "
     # Directory
     prompt-set 40 "&r%~ "
     # New line after the prompt header
