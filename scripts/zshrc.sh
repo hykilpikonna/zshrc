e2ede626b370121ec70848581da19445e0e27187
Azalea Gui <22280294+hykilpikonna@users.noreply.github.com>
2024-11-15 03:02:47 -0500

    [+] mkcd

--- scripts/zshrc.sh
+++ scripts/zshrc.sh
@@ -127,6 +127,15 @@ alias valgrin="valgrind \
   --track-origins=yes \
   --vgdb=no"
 
+mkcd() {
+  if [[ -z $1 ]]; then
+    echo "Usage: mkcd <directory>"
+    return 1
+  fi
+
+  mkdir -p "$1" && cd "$1" || return 1
+}
+
 set-java() {
     eval "$(switch-java "$1")"
 }
