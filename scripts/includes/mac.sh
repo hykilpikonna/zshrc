59e7b5c6aa03a095999828bc4e1f34c8bad792eb
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-10-15 17:12:40 +0800

    [-] conda on mac

--- scripts/includes/mac.sh
+++ scripts/includes/mac.sh
@@ -30,26 +30,6 @@ if [[ $OSTYPE == 'darwin'* ]]; then
         sudo scutil --set ComputerName "$name"
     }
 
-    # Anaconda
-    export CONDA_PATH="/usr/local/anaconda3"
-    export PATH="$CONDA_PATH/bin:$PATH"
-    conda-init() {
-        # >>> conda initialize >>>
-        # !! Contents within this block are managed by 'conda init' !!
-        __conda_setup="$("$CONDA_PATH/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
-        if [ $? -eq 0 ]; then
-            eval "$__conda_setup"
-        else
-            if [ -f "$CONDA_PATH/etc/profile.d/conda.sh" ]; then
-                . "$CONDA_PATH/etc/profile.d/conda.sh"
-            else
-                export PATH="$CONDA_PATH/bin:$PATH"
-            fi
-        fi
-        unset __conda_setup
-        # <<< conda initialize <<<
-    }
-
     # Clear cache to free up disk space
     clean-cache() {
         sudo rm -rf "/Users/hykilpikonna/Library/Caches/Homebrew/downloads"
